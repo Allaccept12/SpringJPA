a65322a07c8e409b54bb9c2a6a572e60d077dda2
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-01-31 23:05:27 +0900

    [ADD] 상품 서비스 및 레포지토리 개발

--- JPA-1/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ JPA-1/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,34 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    /**
+     * 재고 증가
+     * @param stockQuantity
+     */
+    public void addStock(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+    /**
+     * 재고 감소
+     * @param stockQuantity
+     */
+    public void removeStock(int stockQuantity) {
+        int restStock = this.stockQuantity - stockQuantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
+
+
+
+
+
+
+
+
+
+
